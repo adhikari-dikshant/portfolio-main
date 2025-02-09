f74bf619ead32ea9f3cfd1112531bcd4e4058f85
DIKSHANT SINGH ADHIKARI <adhikari.saab03@gmail.com>
2025-02-09 20:02:58 +0530

    portfolio complete

--- src/data.js
+++ src/data.js
@@ -1,41 +1,41 @@
 // projectsData.js
-import ibgroup from '/src/assets/images/ib-group-desktop.webp';
-import memento from '/src/assets/images/memento-desktop.webp';
-import acc from '/src/assets/images/acc-square.webp';
-import daddy from '/src/assets/images/godaddy-desktop.webp';
-import sunnyside from '/src/assets/images/sunny-side-square.webp';
+import cortexMobile from "/src/assets/images/cortex-mobile.webp";
+import cc from "/src/assets/images/codecanvas.webp";
+import formixMobile from "/src/assets/images/formix-mobile.webp";
+import remindMobile from "/src/assets/images/remind-mobile.webp";
+import techdocMobile from "/src/assets/images/techdoc-mobile.webp";
 
 const projectsData = [
-  {
-    title: 'IB Group Vietnam Website',
-    description: 'Web Design / Frontend Development',
-    imageSrc: ibgroup,
-    link: 'https://musical-stroopwafel-1c2327.netlify.app/landing.html'
-  },
-  {
-    title: 'Memento Studio Landing Page',
-    description: 'Web Design / Frontend Development',
-    imageSrc: memento,
-    link: 'https://mementostudio.netlify.app/'
-  },
-  {
-    title: 'Real Business Accountants',
-    description: 'Web Design / Frontend Development',
-    imageSrc: acc,
-    link: 'https://realbusinessaccountants.netlify.app'
-  },
-  {
-    title: 'GoDaddy Landing Page Clone',
-    description: 'Web Design / Frontend Development',
-    imageSrc: daddy,
-    link: 'https://godaddyuiclone.netlify.app'
-  },
-  {
-    title: 'Sunnyside Landing Page',
-    description: 'Web Design / Frontend Development',
-    imageSrc: sunnyside,
-    link: 'https://sunnysidechallenge.netlify.app'
-  }
+    {
+        title: 'Cortex - Notion Clone',
+        description: 'Web Design / Fullstack',
+        imageSrc: cortexMobile,
+        link: 'https://github.com/adhikari-dikshant/notion-clone'
+    },
+    {
+        title: 'Remind Me',
+        description: 'Web Design / Fullstack',
+        imageSrc: remindMobile,
+        link: 'https://remind-me-orpin.vercel.app/'
+    },
+    {
+        title: 'Formix - Automated Form Creator',
+        description: 'Web Design / Fullstack',
+        imageSrc: formixMobile,
+        link: 'https://formix-eta.vercel.app/'
+    },
+    {
+        title: 'Code Canvas - Beautify Code',
+        description: 'Frontend Development',
+        imageSrc: cc,
+        link: 'https://code-canvas-omega.vercel.app/'
+    },
+    {
+        title: 'TechDoc',
+        description: 'Web Design / Frontend Development',
+        imageSrc: techdocMobile,
+        link: 'https://tech-documentation-page-zeta.vercel.app/'
+    }
 ];
 
 export default projectsData;
